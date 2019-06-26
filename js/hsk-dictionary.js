4bb4651191ee3ed8538223c42a4f9dd0f1718a6b
Jon Long <jon.long@voxelcreative.ca>
2019-06-26 05:27:44 -0700

    Add logo

--- js/hsk-dictionary.js
+++ js/hsk-dictionary.js
@@ -2,7 +2,9 @@ function find(word, dictionary) {
   var results = [];
   dictionary.forEach(function(row) {
     if (row['Word'].includes(word)) {
-      results.push(row)
+      if (row['OofC'] == '' && row['PN'] == '') {
+        results.push(row)
+      }
     }
   })
   return results
@@ -19,6 +21,36 @@ function findCharacterExamples(word, dictionary) {
   return characterExamples;
 }
 
+function getImage(entry, app) {
+  $.ajax('img/words/' + entry['Word'] + '.jpg').done(function() {
+    app.image = 'img/words/' + entry['Word'] + '.jpg'
+    app.hasImage = true
+  }).fail(function() {
+    app.unsplashImage = 'https://source.unsplash.com/300x300/?' + entry['English']
+    app.hasImage = false
+  })
+}
+
+function recalculateExampleColumns(word) {
+  var $div = $('.character-example-wrapper > div')
+  var span = 12 / word.length;
+  $div.removeClass()
+  $div.addClass('col-md-' + span)
+}
+
+function show(word, app) {
+  $('#lookup').val(word)
+  app.lookup()
+}
+
+function highlightSentence(entry) {
+  console.log(entry)
+  var sentence = entry['Example']
+  var word = entry['Word']
+  var hsk = entry['Book']
+  $('.example-sentence-word').html(sentence.replace(word, '<b class="hsk' + hsk + '">' + word + '</b>'))
+}
+
 function main(dictionary) {
   var startWord = '固有'
   var entry = find(startWord, dictionary)[0]
@@ -31,18 +63,35 @@ function main(dictionary) {
       characterExamples: characterExamples,
       image: 'img/words/' + entry['Word'] + '.jpg',
       unsplashImage: 'https://source.unsplash.com/300x300/?' + entry['English'],
-      hasImage: true,
-      methods: {
-        lookup: function(word) {
-          this.entry = find(word, dictionary)[0]
-          
+      hasImage: true
+    },
+    methods: {
+      lookup() {
+        var word = $('#lookup').val()
+        var entry = find(word, this.dictionary)[0];
+        if (entry) {
+          this.entry = entry
+          this.characterExamples = findCharacterExamples(word, dictionary)
         }
+        getImage(entry, app)
       }
+    },
+    updated: function() {
+      recalculateExampleColumns(this.entry['Word'])
+      highlightSentence(this.entry)
     }
   })
   $('.show-more').click(function() {
     $(this).parent().find('.character-examples').toggleClass('collapsed')
   })
+  window.onhashchange = function() {
+    word = decodeURI(location.hash.substr(1));
+    show(word, app)
+  }
+  if (location.hash && location.hash.length > 1) {
+    word = decodeURI(location.hash.substr(1));
+    show(word, app)
+  }
 }
 
 Papa.parse('data/HSK Standard Course 1-6-Table 1.csv', {
