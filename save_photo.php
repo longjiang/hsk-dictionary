a24f978ef679719b35799648856a8b47d7e778fd
Jon Long <jon.long@voxelcreative.ca>
2019-07-03 02:11:36 -0700

    save_photo.php

---
+++ save_photo.php
@@ -0,0 +1,21 @@
+<?php
+
+//https://stackoverflow.com/questions/6306935/php-copy-image-to-my-server-direct-from-url/6306995
+function get_photo_and_save($url, $path) {
+	//Get the file
+	$content = file_get_contents($url);
+	//Store in the filesystem.
+	$fp = fopen($path, "w");
+	fwrite($fp, $content);
+	fclose($fp);
+}
+
+echo '<pre>';
+
+$url = $_GET['url'];
+$id = $_GET['id'];
+$word = $_GET['word'];
+$filename = $id . '-' . $word . '.jpg';
+$dir = dirname(__FILE__) . '/img/words/';
+
+get_photo_and_save($url, $dir . $filename);
