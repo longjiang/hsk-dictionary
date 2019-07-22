3eb06ab027c6ee63003f2f6a3cd7eb9fc5406f1d
Jon Long <jon.long@voxelcreative.ca>
2019-07-22 12:20:20 -0700

    Fix collocation collapse button.

--- js/lib/sketch-engine.js
+++ js/lib/sketch-engine.js
@@ -3,6 +3,13 @@ const SketchEngine = {
     $.getJSON(
       `sketch-engine-proxy.php?https://api.sketchengine.eu/bonito/run.cgi/wsketch?corpname=preloaded/zhtenten_lenoch&lemma=${term}`,
       function (response) {
+        if (response.Gramrels && response.Gramrels.length > 0) {
+          response.Gramrels.forEach(function(Gramrel)  {
+            Gramrel.Words = Gramrel.Words.filter(function(Word) {
+              return Word.cm !== ""
+            })
+          })
+        }
         callback(response);
       }
     );
